8781ac4f6c4faa9fa825b1f0ec3b43fce1c30834
chrissx <chrissx@chrissx.ga>
2019-06-09 23:55:42 +0200

    completely eliminated the need for a buffer (i thought direct putchar-ing had some latency)

--- main.c
+++ main.c
@@ -5,41 +5,37 @@
 
 #if defined(_WIN32) || defined(_WIN64)
 #include <Windows.h>
-#define GREEN() SetConsoleTextAttribute(\
+#define green() SetConsoleTextAttribute(\
 	GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN)
-#define OS_SLEEP(ms) Sleep(ms)
+#define os_sleep(ms) Sleep(ms)
 #define width() \
 	CONSOLE_SCREEN_BUFFER_INFO csbi; \
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), \
 		&csbi); \
-	j = csbi.srWindow.Right - csbi.srWindow.Left
+	unsigned int j = csbi.srWindow.Right - csbi.srWindow.Left
 #else
 #include <unistd.h>
 #include <sys/ioctl.h>
-#define GREEN() fputs("\e[32m", stdout)
-#define OS_SLEEP(ms) usleep(ms * 1000)
+#define green() fputs("\e[32m", stdout)
+#define os_sleep(ms) usleep(ms * 1000)
 #define width() \
 	struct winsize w; \
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); \
-	j = w.ws_col
+	unsigned short j = w.ws_col
 #endif
 
 #define assert(condition, msg) if(!condition) { puts(msg); return 1; }
 
 int main(int argc, char **argv)
 {
-	char str[2000]; //buffer
 	srand(time(0));
-	GREEN();
-	int j;
+	green();
 	while(1)
 	{
 		width();
 		assert(j, "width = 0");
-		for(int i = 0; i < j; i++)
-			str[i] = rand() % 10 + 0x30;
-		str[j] = '\0';
-		puts(str);
-		OS_SLEEP(SLEEP);
+		for(int i = 0; i < j; i++) putchar(rand()%10 | 0x30);
+		putchar('\n');
+		os_sleep(SLEEP);
 	}
 }
