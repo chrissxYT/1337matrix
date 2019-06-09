bab47535904ed6b1d62be0c8abd5fbc009a7c939
chrissx <chrissx@chrissx.ga>
2019-06-09 23:10:10 +0200

    shortened the code even more

--- main.c
+++ main.c
@@ -9,24 +9,20 @@
 #define GREEN() SetConsoleTextAttribute(\
 	GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN)
 #define OS_SLEEP(ms) Sleep(ms)
-int width()
-{
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),
-		&csbi);
-	return csbi.srWindow.Right - csbi.srWindow.Left;
-}
+#define width() \
+	CONSOLE_SCREEN_BUFFER_INFO csbi; \
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), \
+		&csbi); \
+	j = csbi.srWindow.Right - csbi.srWindow.Left
 #else
 #include <unistd.h>
 #include <sys/ioctl.h>
 #define GREEN() fputs("\e[32m", stdout)
 #define OS_SLEEP(ms) usleep(ms * 1000)
-int width()
-{
-	struct winsize w;
-	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-	return w.ws_col;
-}
+#define width() \
+	struct winsize w; \
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); \
+	j = w.ws_col
 #endif
 
 #define DIE(msg) { puts(msg); return 1; }
@@ -34,19 +30,17 @@ int width()
 int main(int argc, char **argv)
 {
 	char *str = malloc(2000); //buffer
-	if (!str) DIE("WTF, MALLOC FAILS");
+	if (!str) DIE("WTF, MALLOC FAILS!?");
 	srand(time(0));
 	GREEN();
 	int j;
 	while (1)
 	{
-		j = width();
+		width();
 		for (int i = 0; i < j; i++)
-			str[i] = rand() % 10 + 48;
+			str[i] = rand() % 10 + 0x30;
 		str[j] = '\0';
 		puts(str);
-#if SLEEP
 		OS_SLEEP(SLEEP);
-#endif
 	}
 }
